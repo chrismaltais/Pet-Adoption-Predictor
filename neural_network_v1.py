59ece9a45682e73a9e75d787e64d649f01d43a22
Chris Maltais <chris.maltais@hotmail.ca>
2019-03-23 15:28:51 -0400

    ✨ Add parsing of sentiment json

--- neural_network_v1.py
+++ neural_network_v1.py
@@ -1,3 +1,5 @@
+import os
+import json
 import pandas as pd
 import numpy as np
 # Import the model
@@ -6,52 +8,77 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from matplotlib import pyplot as plt
+
+# Suppress Warnings
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+
+# Returns Dataframe of Pet_ID, magnitude, score
+def get_sentiment_values():
+    data = []
+    
+    # df = pd.DataFrame(index=columns[0], columns=columns)
+    for filename in os.listdir('data/test_sentiment'):
+        # Get magnitude and score
+        with open('data/test_sentiment/' + filename) as json_file:
+            contents = json.load(json_file)
+            magnitude = contents["documentSentiment"]["magnitude"]
+            score = contents["documentSentiment"]["score"]
+        row = []
+        # Get PetID
+        filename_split = filename.split(".")
+        petID = filename_split[0]
+        row.append(petID)
+        row.append(magnitude)
+        row.append(score)
+        data.append(row)
+    # Convert to DataFrame
+    columns = ['PetID', 'Magnitude', 'Score']
+    results = pd.DataFrame(data=data, columns=columns)
+    results.set_index('PetID')
+    return results
     
 if __name__ == "__main__":
     # Get Training Data
-    dataset_train = pd.read_csv('data/train.csv')
-    y_value_train = dataset_train['AdoptionSpeed']
-    # Train based on: Type, Age, Breed1, Breed2, Gender, Color1, Color2, Color3
-    columns_to_drop = ['Name', 'State', 'RescuerID', 'Description', 'PetID', 'AdoptionSpeed'] #PhotoAmt, VideoAmt
-    x_value_train = dataset_train.drop(columns_to_drop, axis=1)
-
-    x_train, x_test, y_train, y_test = train_test_split(x_value_train, y_value_train, test_size=0.20, stratify=y_value_train)
+    x_train = pd.read_csv('x_train.csv', index_col=0)
+    y_train = pd.read_csv('y_train.csv', index_col=0)
+    # Get Testing Data
+    x_test = pd.read_csv('x_test.csv', index_col=0)
+    y_test = pd.read_csv('y_test.csv', index_col=0)
     
     # Find number of each type of adoption
     # print(len(dataset_train[dataset_train.AdoptionSpeed == 0]))
 
-    plt.matshow(x_value_train.corr())
-    plt.show()
-
-
-    # sc = StandardScaler()
-    # sc.fit(x_train)
-
-    # x_train_std = sc.transform(x_train)
-    # x_test_std = sc.transform(x_test)
+    # Show correlation table
+    # plt.matshow(x_train.corr())
+    # plt.show()
+    get_sentiment_values()
 
     # Initializing the multilayer perceptron
-    mlp = MLPClassifier(hidden_layer_sizes=(2), solver='sgd', learning_rate_init= 0.01, max_iter=500)
+    # mlp = MLPClassifier(hidden_layer_sizes=(2), solver='sgd', learning_rate_init= 0.01, max_iter=500)
 
-    # Fit the model
-    # mlp.fit(x_train_std, y_train)
-    mlp.fit(x_train, y_train)
+    # # Fit the model
+    # mlp.fit(x_train, y_train)
 
-    predicted_results = mlp.predict(x_test)
+    # predicted_results = mlp.predict(x_test)
 
-    print("Actual Values: ", y_test.values)
+    # print("Actual Values: ", y_test['AdoptionSpeed'].values)
     
-    print("Predicted Values: ", predicted_results)
+    # print("Predicted Values: ", predicted_results)
 
-    print(mlp.score(x_test, y_test))
+    # print("Score: ", mlp.score(x_test, y_test))
 
-    # Confusion Matrix
-    confusion_matrix_results = confusion_matrix(y_test.values, predicted_results)
-    print(confusion_matrix_results)
+    # get_sentiment_values()
 
-    # Classification Report
-    class_report_results = classification_report(y_test.values, predicted_results)
-    print(class_report_results)
+    # # Confusion Matrix
+    # confusion_matrix_results = confusion_matrix(y_test.values, predicted_results)
+    # print(confusion_matrix_results)
+
+    # # Classification Report
+    # class_report_results = classification_report(y_test.values, predicted_results)
+    # print(class_report_results)
 
     # Get Score
     #print(mlp.score(x_test_std, y_test))
@@ -62,10 +89,6 @@ if __name__ == "__main__":
     # Number 1 = 3090
     # Number 0 = 410
 
-    # Add stratification
-    # Duplicate 0 rows 
-    # Truncate others 
-
     # PCA
     # Correlation Matrix
 
@@ -74,11 +97,6 @@ if __name__ == "__main__":
     # Handling JSON Data:
     # Create dataframe w magnitude and score and pet_id
     # Every row might not have 
-
-    # Questions to Prof:
-    # Average
-    # One final big model
-    # Take each NN as an input and get result
 
 
 
