56b5b3fb732fad8de32a8ccc581aa1ce7fad7e2c
Chris Maltais <chris.maltais@hotmail.ca>
2019-03-25 18:53:14 -0400

    ✨ Add hyper parameter tuning file

--- neural_network_v1.py
+++ neural_network_v1.py
@@ -2,6 +2,7 @@ import os
 import json
 import pandas as pd
 import numpy as np
+import sys
 # Import the model
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
@@ -18,8 +19,6 @@ warnings.warn = warn
 # Returns Dataframe of Pet_ID, magnitude, score
 def get_sentiment_values(folderpath):
     data = []
-    
-    # df = pd.DataFrame(index=columns[0], columns=columns)
     for filename in os.listdir(folderpath):
         # Get magnitude and score
         with open(folderpath + '/' + filename) as json_file:
@@ -39,15 +38,96 @@ def get_sentiment_values(folderpath):
     results = pd.DataFrame(data=data, columns=columns)
     results.set_index('PetID', inplace=True)
     return results
-    
+
+def get_ideal_HP(x_train, x_test, y_train, y_test, max_iterations, HL_size, learning_rate_init, learning_rate, momentum):
+    hyper_params = {
+        "accuracy": 0,
+        "kappa": 0,
+        "HL_size": 0, 
+        "learning_rate_init": 0,
+        "learning_rate": 0, 
+        "momentum": 0
+    }
+
+    max_kappa = 0
+
+    for layer_size in HL_size:
+        for init_LR in learning_rate_init:
+            for LR in learning_rate:
+                for mu in momentum:
+                    print(layer_size)
+                    print(init_LR)
+                    print(LR)
+                    print(mu)
+                    # Initializing the multilayer perceptron
+                    mlp = MLPClassifier(
+                        hidden_layer_sizes=(layer_size), 
+                        solver='sgd', 
+                        learning_rate_init= init_LR, 
+                        learning_rate=LR,
+                        momentum=mu, 
+                        max_iter=500
+                    )
+                    # Fit the model
+                    mlp.fit(x_train, y_train)
+
+                    # Predicted Results
+                    predicted_results = mlp.predict(x_test)
+
+                    # Accuracy and Kappa Scores
+                    accuracy = mlp.score(x_test, y_test)
+                    kappa = cohen_kappa_score(y_test.values, predicted_results, weights = 'quadratic')
+
+                    # Write to file
+                    write_HP_to_file(accuracy, kappa, layer_size, init_LR, LR, mu)
+
+                    print("Accuracy: ", accuracy)
+                    print("Weighted Kappa: ", kappa)
+
+                    if (kappa > max_kappa):
+                        max_kappa = kappa
+                        hyper_params = {
+                            "accuracy": accuracy,
+                            "kappa": kappa,
+                            "HL_size": layer_size, 
+                            "learning_rate_init": init_LR,
+                            "learning_rate": LR, 
+                            "momentum": mu
+                        }
+    return hyper_params
+
+def write_HP_to_file(accuracy, kappa, layer_size, learning_rate_init, learning_rate, momentum):
+    filename = 'results/hyperparameter_tuning.txt'
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    with open(filename, 'a') as f:
+        f.write(
+            "Accuracy: {accuracy}, Weighted Kappa: {kappa}, Hidden Layer Size: {layer_size}, Initial Learning Rate: {learning_rate_init}, Learning Rate: {learning_rate}, Momentum: {momentum}\n"
+            .format(accuracy=accuracy, kappa=kappa, layer_size=layer_size, learning_rate_init=learning_rate_init, learning_rate=learning_rate, momentum=momentum)
+        )
+
 if __name__ == "__main__":
     # Get Training Data
     x_train = pd.read_csv('x_train.csv', index_col=0)
     y_train = pd.read_csv('y_train.csv', index_col=0)
-    
+    print(x_train.shape)
     # Get Testing Data
     x_test = pd.read_csv('x_test.csv', index_col=0)
     y_test = pd.read_csv('y_test.csv', index_col=0)
+
+    # Check if useNLP flag is true
+    if (len(sys.argv) == 2):
+        if(sys.argv[1] == "useNLP"):
+            # Extract sentiment from JSON files
+            train_sentiment = get_sentiment_values('data/train_sentiment')
+    
+            # Add sentiment to train and test data
+            x_train = x_train.merge(train_sentiment, on='PetID', how='left')
+            x_test = x_test.merge(train_sentiment, on='PetID', how='left')
+
+            x_train.replace(to_replace=float('NaN'), value=0, inplace=True)
+            x_test.replace(to_replace=float('NaN'), value=0, inplace=True)
+            
+    
     
     # Find number of each type of adoption
     # print(len(dataset_train[dataset_train.AdoptionSpeed == 0]))
@@ -56,21 +136,41 @@ if __name__ == "__main__":
     # plt.matshow(x_train.corr())
     # plt.show()
 
-    # Extract sentiment from JSON files
-    train_sentiment = get_sentiment_values('data/train_sentiment')
+    # Tune hyper-parameters
+    # HL_size = [2, 5, 10, 15, 50, 100]
+    # learning_rate_init = [0.001, 0.01, 0.05, 0.1, 0.5, 0.9]
+    # learning_rate = ['constant', 'adaptive']
+    # momentum = [0.1, 0.3, 0.5, 0.7, 0.9]
     
-    # Add sentiment to train and test data
-    x_train = x_train.merge(train_sentiment, on='PetID', how='left')
-    x_test = x_test.merge(train_sentiment, on='PetID', how='left')
+    # ideal_HP = get_ideal_HP(
+    #     x_train, 
+    #     x_test, 
+    #     y_train, 
+    #     y_test, 
+    #     500, 
+    #     HL_size, 
+    #     learning_rate_init, 
+    #     learning_rate, 
+    #     momentum
+    # )
 
-    x_train.replace(to_replace=float('NaN'), value=0, inplace=True)
-    x_test.replace(to_replace=float('NaN'), value=0, inplace=True)
+    # print(ideal_HP)
 
     # Initializing the multilayer perceptron
-    mlp = MLPClassifier(hidden_layer_sizes=(10), solver='sgd', learning_rate_init= 0.01, max_iter=500)
+    mlp = MLPClassifier(
+        hidden_layer_sizes=(5), 
+        solver='sgd', 
+        learning_rate_init= 0.05, 
+        learning_rate='constant', 
+        momentum=0.5, 
+        max_iter=500
+    )
 
     # Fit the model
     mlp.fit(x_train, y_train)
+
+    print(mlp.out_activation_)
+    print(mlp.n_layers_)
 
     predicted_results = mlp.predict(x_test)
 
@@ -100,11 +200,6 @@ if __name__ == "__main__":
     # PCA
     # Correlation Matrix
 
-    # Not normalize because data is categorical
-
-    # Handling JSON Data:
-    # Create dataframe w magnitude and score and pet_id
-    # Every row might not have 
 
 
 
